a8db603e63186167a4c51698354ee704271674a1
nate <gothnatec@gmail.com>
2022-11-10 14:22:20 -0500

    Scanner

--- test.java
+++ test.java
@@ -1,3 +1,6 @@
+import java.util.Scanner;
 public class test {
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+    }
 }
